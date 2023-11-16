0b1b79a07eb6eecc217b7e2ec9ea47d16f5849de
skaimarvas <skaimarvas@gmail.com>
2023-11-16 19:40:42 +0300

    Tüm testler yapıldı.

--- frontend/components/IletisimFormu.test.js
+++ frontend/components/IletisimFormu.test.js
@@ -9,6 +9,7 @@ import {
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
 import IletisimFormu from "./IletisimFormu";
+import Goruntule from "./Goruntule";
 
 test("hata olmadan render ediliyor", () => {
   render(<IletisimFormu />);
@@ -24,8 +25,12 @@ test("iletişim formu headerı render ediliyor", () => {
 
 test("kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render ediyor.", async () => {
   render(<IletisimFormu />);
-  const username = screen.getByTestId("name");
-  userEvent.type(username, "İlh");
+
+  await waitFor(() => {
+    const username = screen.getByTestId("name");
+    userEvent.type(username, "İlh");
+  });
+
   const hata = screen.getByTestId("error");
   expect(hata).toHaveTextContent("ad en az 5 karakter olmalıdır.");
   screen.debug(hata);
@@ -33,6 +38,7 @@ test("kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render 
 
 test("kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyor.", async () => {
   render(<IletisimFormu />);
+
   const gonder = screen.getByTestId("submit");
   const hata1 = screen.queryByTestId("error");
   expect(hata1).not.toBeInTheDocument();
@@ -58,12 +64,103 @@ test("kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyo
   //forEach methodu ile hataları içerip içermediğini kontrol ettik
 });
 
-test("kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.", async () => {});
+test("kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.", async () => {
+  render(<IletisimFormu />);
+  const username = screen.getByTestId("name");
+  userEvent.type(username, "İlhan");
+  const usersurname = screen.getByTestId("surname");
+  userEvent.type(usersurname, "Mansız");
 
-test('geçersiz bir mail girildiğinde "email geçerli bir email adresi olmalıdır." hata mesajı render ediliyor', async () => {});
+  const gonder = screen.getByTestId("submit");
+  const hata1 = screen.queryByTestId("error");
+  expect(hata1).not.toBeInTheDocument();
+  //butona basmadan önce hata mesajı olup olmadığını kontrol ediyoruz.
+  await waitFor(() => {
+    userEvent.click(gonder);
+  });
 
-test('soyad girilmeden gönderilirse "soyad gereklidir." mesajı render ediliyor', async () => {});
+  const hata = screen.getByTestId("error");
+  expect(hata).toHaveTextContent("email geçerli bir email adresi olmalıdır.");
+  screen.debug(hata);
+});
 
-test("ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.", async () => {});
+test('geçersiz bir mail girildiğinde "email geçerli bir email adresi olmalıdır." hata mesajı render ediliyor', async () => {
+  render(<IletisimFormu />);
 
-test("form gönderildiğinde girilen tüm değerler render ediliyor.", async () => {});
+  await waitFor(() => {
+    const email = screen.getByTestId("email");
+    userEvent.type(email, "asd");
+  });
+
+  const hata = screen.getByTestId("error");
+  expect(hata).toHaveTextContent("email geçerli bir email adresi olmalıdır");
+  screen.debug(hata);
+});
+
+test('soyad girilmeden gönderilirse "soyad gereklidir." mesajı render ediliyor', async () => {
+  render(<IletisimFormu />);
+  const username = screen.getByTestId("name");
+  userEvent.type(username, "İlhan");
+  const useremail = screen.getByTestId("email");
+  userEvent.type(useremail, "abc@abc.com");
+
+  const gonder = screen.getByTestId("submit");
+  const hata1 = screen.queryByTestId("error");
+  expect(hata1).not.toBeInTheDocument();
+  //butona basmadan önce hata mesajı olup olmadığını kontrol ediyoruz.
+  await waitFor(() => {
+    userEvent.click(gonder);
+  });
+
+  const hata = screen.getByTestId("error");
+  expect(hata).toHaveTextContent("soyad gereklidir.");
+  screen.debug(hata);
+});
+
+test("ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.", async () => {
+  render(<IletisimFormu />);
+  const username = screen.getByTestId("name");
+  userEvent.type(username, "İlhan");
+  const usersurname = screen.getByTestId("surname");
+  userEvent.type(usersurname, "Mansız");
+  const useremail = screen.getByTestId("email");
+  userEvent.type(useremail, "abc@abc.com");
+
+  const gonder = screen.getByTestId("submit");
+  await waitFor(() => {
+    userEvent.click(gonder);
+  });
+
+  const hata = screen.queryByTestId("error");
+  expect(hata).not.toBeInTheDocument();
+
+  screen.debug(hata);
+});
+
+test("form gönderildiğinde girilen tüm değerler render ediliyor.", async () => {
+  render(<IletisimFormu />);
+  const username = screen.getByTestId("name");
+  userEvent.type(username, "İlhan");
+  const usersurname = screen.getByTestId("surname");
+  userEvent.type(usersurname, "Mansız");
+  const useremail = screen.getByTestId("email");
+  userEvent.type(useremail, "abc@abc.com");
+  const message = screen.getByTestId("mesaj");
+  userEvent.type(message, "deneme");
+
+  const gonder = screen.getByTestId("submit");
+  await waitFor(() => {
+    userEvent.click(gonder);
+  });
+
+  const firstname = screen.getByTestId("firstnameDisplay");
+  expect(firstname).toHaveTextContent("İlhan");
+  const lastname = screen.getByTestId("lastnameDisplay");
+  expect(lastname).toHaveTextContent("Mansız");
+  const email = screen.getByTestId("emailDisplay");
+  expect(email).toHaveTextContent("abc@abc.com");
+  const messageD = screen.getByTestId("messageDisplay");
+  expect(messageD).toHaveTextContent("deneme");
+});
+
+// await waitFor işlemini genellike gönder butonuna ekledim. Çünkü asenkron işlem gerçekleştiriyor. Gönder işlemini yapmadan test işlemlerinde hata çıkmasına neden olabilir.
